572e9d9bfe27f560ff1a819398e3485218dadbc7
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2023-10-02 09:22:13 -0400

    sd

---
+++ app.js
@@ -0,0 +1,29 @@
+//declare and defince function, input output machines
+//we can pass in arguements or parameters
+function test(mess) {
+    console.log(mess);
+}
+//run the function
+test("Hi");
+
+//String ""
+const x = "String";
+//declaring a variable, defining a variable
+test(x);
+
+//Number/Integer
+const y = 98;
+test(y);
+
+//Boolean true or false
+const z = true;
+test(z);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+let a = Number(prompt("Enter a number"));
+let b = Number(prompt("Enter another number:"));
+add(a, b);
+
+
